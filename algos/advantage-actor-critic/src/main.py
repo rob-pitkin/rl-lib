4e6db836f618d930f2b0ffb058ffa71495afcfca
Rob Pitkin <43574523+rob-pitkin@users.noreply.github.com>
2025-10-03 01:25:56 -0700

    train for longer

--- algos/advantage-actor-critic/src/main.py
+++ algos/advantage-actor-critic/src/main.py
@@ -4,9 +4,9 @@ from agent import AdvantageActorCriticAgent
 
 def main():
     env = gymnasium.make("LunarLander-v3")
-    agent = AdvantageActorCriticAgent(env, net_arch={}, update_frequency=100)
+    agent = AdvantageActorCriticAgent(env, net_arch={}, update_frequency=256)
     agent.train(
-        1000000,
+        2000000,
     )
     agent.save_model(
         "algos/advantage-actor-critic/src/a2c_actor.pt",
